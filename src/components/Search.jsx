44313edd848735c23f43218db1bfbecc92d872ce
Aboli Wankhade <104322603+Aboli56@users.noreply.github.com>
2022-09-02 13:27:50 +0530

    Update Search.jsx

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Search = ({handleSearchNote}) => {
   return (
     <div className='search'>
-        <h2 className='search-icon'>@</h2>
+        <h2 className='search-icon'>🔍</h2>
         <input type="text" onChange={(e)=>handleSearchNote(e.target.value)} placeholder="Type to Serach...."/>
     </div>
   )
