b51a5016dea354d9db28252bdc23c63745c48a99
Aboli Wankhade <104322603+Aboli56@users.noreply.github.com>
2022-09-02 17:50:33 +0530

    Update Note.jsx

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -6,7 +6,7 @@ const Note = ({id , text, date, handleDeleteNote}) => {
       <span>{text}</span>
       <div className='note-footer'>
         <small>{date}</small>
-        <button className='save' onClick={()=>handleDeleteNote(id)}>Delete</button>
+        <button className='delete-btn' onClick={()=>handleDeleteNote(id)}>Delete</button>
       </div>
     </div>
   )
